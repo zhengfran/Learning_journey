6c5c046d0d9dabdad0db7caf224d7c1757cc68ec
Zheng Zhicheng <zhengzc8270@jkts.jvckenwood.com>
2019-11-13 16:56:28 +0800

    11.13 update

---
+++ LeetCode/String/22.generating_parentheses.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        int bal=0;
+        for (int i=0;i<2*n;i++){
+            if (bal==0){
+                //this looks like an brutal force solution, need to think of a better one
+            }
+        }
+        
+    }
+};
