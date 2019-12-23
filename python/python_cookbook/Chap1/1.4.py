3c748951ade10a10cb5b54cf96a452fd98bc194f
Zheng Zhicheng <zhengzc8270@jkts.jvckenwood.com>
2019-12-23 14:12:41 +0800

    folder restructure

---
+++ python/python_cookbook/Chap1/1.4.py
@@ -0,0 +1,7 @@
+#Make a list of the largest or smallest N items in a collection
+
+import heapq
+
+nums = [1,8,2,23,7,-4,18,23,42,37,2]
+print(heapq.nlargest(3,nums))
+print(heapq.nsmallest(3,nums))
